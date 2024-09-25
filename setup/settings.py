acb20362e148dfb8aee266e731701718fc6ac10a
Felipe Falleiros <mendesf84@gmail.com>
2024-09-25 17:36:21 -0300

    finalizacao do projeto

--- setup/settings.py
+++ setup/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# indica onde os arquivos estáticos estão
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+# caminho absoluto pro diretório onde o python vai coletar os arquivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
